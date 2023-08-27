c824c532486f685f730a712eff515ad5ecc2af1c
Brioche <thomasderudder@sfr.fr>
2023-08-27 15:57:54 +0200

    add api like fastapi template

--- Backend/api/target/debug/api.d
+++
@@ -1,1 +0,0 @@
-/home/brioche/Documents/04_ASSO/ExpenseTracker/Back-Connection/Backend/api/target/debug/api: /home/brioche/Documents/04_ASSO/ExpenseTracker/Back-Connection/Backend/api/src/main.rs
--- Backend/api/target/debug/deps/api-9e6d3d6a5044d931.d
+++
@@ -1,5 +0,0 @@
-/home/brioche/Documents/04_ASSO/ExpenseTracker/Back-Connection/Backend/api/target/debug/deps/api-9e6d3d6a5044d931.rmeta: src/main.rs
-
-/home/brioche/Documents/04_ASSO/ExpenseTracker/Back-Connection/Backend/api/target/debug/deps/api-9e6d3d6a5044d931.d: src/main.rs
-
-src/main.rs:
